ea014947d80c030f7f15b4e5a63ce74751c64801
MaxiArrettino <maximilianoarrettino@gmail.com>
2020-12-20 20:33:14 -0300

    Changed header style

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 export default function Header() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a className="navbar-brand" href="/">
+      <a className="navbar-brand ml-4" href="/">
         Home
       </a>
       <ul className="navbar-nav ml-auto">
         <li>
-          <a className="nav-link" href="/create">
+          <a className="nav-link mr-4" href="/create">
             Create new post
           </a>
         </li>
