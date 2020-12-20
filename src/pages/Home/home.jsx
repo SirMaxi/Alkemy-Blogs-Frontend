8c3537c5e459ae0e720e562d030933834f0618b5
MaxiArrettino <maximilianoarrettino@gmail.com>
2020-12-20 20:25:01 -0300

    Changed some styles

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import Posts from '../../components/Posts/Posts';
+import Pagination from '../../components/Pagination/Pagination';
 
 function home() {
   const [posts, setPosts] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(10);
 
   const list = async () => {
     try {
@@ -23,6 +26,14 @@ function home() {
   if (!posts) {
     return <div>No hay listado por el momento</div>;
   }
+
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <div>
@@ -45,8 +56,13 @@ function home() {
                   <th scope="col"> </th>
                 </tr>
               </thead>
-              <Posts postsData={posts} />
+              <Posts postsData={currentPosts} />
             </table>
+            <Pagination
+              postsPerPage={postsPerPage}
+              totalPosts={posts.length}
+              paginate={paginate}
+            />
           </div>
         </div>
       </div>
