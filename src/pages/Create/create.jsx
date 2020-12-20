df5f87f7fa3ae88d5385fae979a6669850813dd6
MaxiArrettino <maximilianoarrettino@gmail.com>
2020-12-20 19:10:14 -0300

    Create page in wich you can create your post

---
+++ src/pages/Create/create.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import './create.css';
+
+function create() {
+  const [data, setData] = useState({
+    title: '',
+    body: '',
+  });
+
+  const onChange = (e) => {
+    console.log(e);
+    const name = e.target.id;
+    const { value } = e.target;
+    console.log(name);
+    console.log(value);
+    let newData = data;
+    newData = { ...newData, [name]: value };
+    setData(newData);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!data.title || !data.body) {
+      alert('All fields are required');
+      return;
+    }
+    const sendData = data;
+    try {
+      await fetch(`${process.env.REACT_APP_BASE_URL}/addoperation`, {
+        method: 'POST',
+        body: JSON.stringify(sendData),
+        headers: {
+          'Content-type': 'application/json',
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    alert('New operation added');
+    window.location.reload();
+  };
+  return (
+    <div className="custom-container">
+      <h2 className="text-center">Create a new post</h2>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            value={data.title}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="body">Body</label>
+          <input
+            type="text"
+            className="form-control"
+            id="body"
+            value={data.body}
+            onChange={onChange}
+          />
+        </div>
+        <div className="text-center">
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={onSubmit}
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default create;
