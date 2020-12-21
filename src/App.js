d9bd469d474e48dab56e51276e72e143876e5227
MaxiArrettino <maximilianoarrettino@gmail.com>
2020-12-21 18:55:21 -0300

    Deleted details route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from './components/Header/header';
 import Home from './pages/Home/home';
-import Details from './pages/Detail/detail';
 import Create from './pages/Create/create';
 import Update from './pages/Update/update';
 import Post from './pages/Post/post';
@@ -14,7 +13,6 @@ function App() {
       <Header />
       <Switch>
         <Route exact path="/post" component={Post} />
-        <Route exact path="/details" component={Details} />
         <Route exact path="/create" component={Create} />
         <Route exact path="/update" component={Update} />
         <Route path="/" component={Home} />
