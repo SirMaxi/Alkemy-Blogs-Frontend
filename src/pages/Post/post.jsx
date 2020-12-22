50bbcab778d9cc2d13e061d1db29ca0bf94e7387
MaxiArrettino <maximilianoarrettino@gmail.com>
2020-12-22 12:45:09 -0300

    Added Image for the card

--- src/pages/Post/post.jsx
+++ src/pages/Post/post.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
+import './post.css';
 
 function post(props) {
-  const { data } = props.location.state;
-  const postData = data[0];
+  const data = props.location.state.data;
+
   return (
     <div className="container">
       <h2 className="text-center" style={{ margin: '30px 0px' }}>
         Post Details
       </h2>
       <div className="card shadow">
+        {data.image ? (
+          <img
+            className="card-img-top card-header p-0"
+            src={data.image}
+            alt="Card image"
+          />
+        ) : null}
         <div className="card-header">
-          <h4 className="card-title text-center">{postData.title}</h4>
+          <h4 className="card-title text-center">{data.title}</h4>
         </div>
         <div className="card-body">
-          <p className="card-text">{postData.body}</p>
+          <p className="card-text text-center">{data.body}</p>
           <a href="/">Back to Home</a>
         </div>
       </div>
